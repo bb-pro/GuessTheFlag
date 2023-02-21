fd8f7d84f3ebc37bc0e1ff197fe2317a9dc470a7
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-02-21 14:07:07 +0500

    work with model

--- GuessTheFlag/ViewControllers/QuizViewController.swift
+++ GuessTheFlag/ViewControllers/QuizViewController.swift
@@ -6,26 +6,66 @@
 //
 
 import UIKit
-
 final class QuizViewController: UIViewController {
-
-    @IBOutlet var flagImage: UIImageView!
     
+    @IBOutlet var flagImage: UIImageView!
+    @IBOutlet var flagButtons: [UIButton]!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    private var countryName: String!
     private let flags = FlagManager.parseJson()
-
+    private var scoreCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUI()
     }
-
+    
+    @IBAction func AnswerButtonPressed(_ sender: UIButton) {
+        checkAnswer(sender: sender)
+        updateUI(with: sender)
+        
+        print(" user's choice \(sender.currentTitle)")
+        
+    }
+    
 }
 
-//Private Methods
 
+
+//MARK: - Private Methods
 private extension QuizViewController {
-    func updateUI() {
+    func updateUI(with sender: UIButton) {
         let number = Int.random(in: 0...100)
         flagImage.image = UIImage(named: flags[number].code.lowercased())
+        countryName = String(flags[number].country)
+        
+        updateButtonTitles(answer: countryName, with: flags)
+        checkAnswer(sender: sender)
+    }
+    
+    func updateButtonTitles(answer: String, with options: [Flag]) {
+        var answerOptions = [answer]
+        
+        for _ in 1...3 {
+            if let country = options.randomElement()?.country {
+                answerOptions.append(country)
+            }
+            
+            
+        }
+        
+        let answers = Set(answerOptions)
+        for (button, answer) in zip(flagButtons, answers) {
+            button.setTitle(answer, for: .normal)
+        }
+    }
+    func checkAnswer(sender: UIButton) {
+        guard let countryTitle = sender.currentTitle else { return }
+        if countryTitle == countryName {
+            scoreCount += 1
+            scoreLabel.text = String(scoreCount)
+        }
     }
 }
+
+
