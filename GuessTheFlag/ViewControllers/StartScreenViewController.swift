7f41205b0de6d91335fde9344548bc8870252c71
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-02-23 01:21:03 +0500

    work with UI

--- GuessTheFlag/ViewControllers/StartScreenViewController.swift
+++ GuessTheFlag/ViewControllers/StartScreenViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 final class StartScreenViewController: UIViewController {
 
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addVerticalGradientLayer()
+        buttons.forEach { button in
+            button.layer.cornerRadius = 15
+        }
     }
 
 }
