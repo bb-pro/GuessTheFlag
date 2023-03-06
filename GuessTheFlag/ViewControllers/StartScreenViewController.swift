20e6c98adf56a58f4139ca2b843d73c33f881a38
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-03-06 20:07:21 +0500

    work with ui

--- GuessTheFlag/ViewControllers/StartScreenViewController.swift
+++ GuessTheFlag/ViewControllers/StartScreenViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 final class StartScreenViewController: UIViewController {
 
+    @IBOutlet var image: UIImageView!
     
     @IBOutlet var buttons: [UIButton]!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        image.layer.cornerRadius = image.frame.width / 2
         let defaults = UserDefaults.standard
         defaults.set(0, forKey: "Record")
-        super.viewDidLoad()
         buttons.forEach { button in
             button.layer.cornerRadius = 15
         }
